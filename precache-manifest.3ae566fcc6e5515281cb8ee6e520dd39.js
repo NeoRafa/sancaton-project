06ab6909e32f6bfe5021a4722a89accaf96a659b
Rafael Pili <hpcponte@gmail.com>
2020-05-03 19:52:14 -0300

    Updates

---
+++ precache-manifest.3ae566fcc6e5515281cb8ee6e520dd39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2248d211616558a2faab71da220ffe4",
+    "url": "/sancaton-project/index.html"
+  },
+  {
+    "revision": "fa09e7277ec76e73c607",
+    "url": "/sancaton-project/static/css/2.680b4795.chunk.css"
+  },
+  {
+    "revision": "b3251040b99538f0a216",
+    "url": "/sancaton-project/static/css/main.56bef904.chunk.css"
+  },
+  {
+    "revision": "fa09e7277ec76e73c607",
+    "url": "/sancaton-project/static/js/2.b4a56c62.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/sancaton-project/static/js/2.b4a56c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3251040b99538f0a216",
+    "url": "/sancaton-project/static/js/main.06268c90.chunk.js"
+  },
+  {
+    "revision": "d00852cc4bb2db2bef64",
+    "url": "/sancaton-project/static/js/runtime-main.e3df35d6.js"
+  }
+]);
